c276ee2bb2ea3af7ed1860b7e1718b24ef6b5459
abakarmagomedov <skat131998@mail.ru>
2017-05-30 17:57:29 +0300

    new request is added

--- app/src/main/java/com/smedialink/abakarmagomedov/mvpyandextranslator/data/net/YandexApi.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/mvpyandextranslator/data/net/YandexApi.java
@@ -16,4 +16,9 @@ public interface YandexApi {
 
     @GET("api/v1.5/tr.json/translate?")
     Observable<Translate> getTranslate(@QueryMap HashMap<String, String> map);
+
+
+    //@GET("/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20170504T104836Z.704d64c90ed3fc09.0c073c975fdca419ff33264c65a2fd744454e99b")
+    //Observable<> getLanguages();
+
 }
