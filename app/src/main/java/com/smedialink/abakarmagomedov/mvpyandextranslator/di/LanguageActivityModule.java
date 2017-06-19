b73fcfe83de66edfd8ca85d64df7c5bfd8200329
abakarmagomedov <skat131998@mail.ru>
2017-06-19 18:58:10 +0300

    work with di of language activity

--- app/src/main/java/com/smedialink/abakarmagomedov/mvpyandextranslator/di/LanguageActivityModule.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/mvpyandextranslator/di/LanguageActivityModule.java
@@ -1,13 +1,31 @@
 package com.smedialink.abakarmagomedov.mvpyandextranslator.di;
 
+import com.smedialink.abakarmagomedov.mvpyandextranslator.data.repository.LanguagesRepository;
 import com.smedialink.abakarmagomedov.mvpyandextranslator.di.base.LogicModule;
+import com.smedialink.abakarmagomedov.mvpyandextranslator.presentation.language_choose.LanguageInteractor;
+import com.smedialink.abakarmagomedov.mvpyandextranslator.presentation.language_choose.LanguageInteractorImp;
+import com.smedialink.abakarmagomedov.mvpyandextranslator.presentation.language_choose.LanguagePresenter;
+import com.smedialink.abakarmagomedov.mvpyandextranslator.presentation.language_choose.LanguagePresenterImp;
+
+import dagger.Provides;
 
 /**
  * Created by abakarmagomedov on 19/06/17.
  */
 
 
+@LanguageScope
 public class LanguageActivityModule implements LogicModule {
 
+    @LanguageScope
+    @Provides
+    LanguagePresenter provideLanguagePresenter(LanguageInteractor interactor){
+        return  new LanguagePresenterImp(interactor);
+    }
 
+    @LanguageScope
+    @Provides
+    LanguageInteractor provideLanguageInteractor(LanguagesRepository repository){
+        return new LanguageInteractorImp(repository);
+    }
 }
